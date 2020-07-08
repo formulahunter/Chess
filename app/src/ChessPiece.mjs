1b6ef0a9b8305580973609b269689d165f476b7f
formulahunter <gayden.hunter@gmail.com>
2020-07-08 12:34:40 -0400

    Define possible moves based on type & position
    The ChessPiece.possibleMoves property enumerates movements that the
    given piece may make, accounting for its current position by excluding
    those that would move outside the board's bounds.

--- app/src/ChessPiece.mjs
+++ app/src/ChessPiece.mjs
@@ -1,6 +1,152 @@
 import * as CONST from './constants.mjs';
 
 
+/** all possible movements (regardless of position) by type */
+const MOVEMENTS = {
+    'KING':  [
+        [1, 0],
+        [0, 1],
+        [-1, 0],
+        [0, -1],
+        [1, 1],
+        [1, -1],
+        [-1, 1],
+        [-1, -1],
+    ],
+    'QUEEN': [
+        [1, 0],
+        [2, 0],
+        [3, 0],
+        [4, 0],
+        [5, 0],
+        [6, 0],
+        [7, 0],
+        [0, 1],
+        [0, 2],
+        [0, 3],
+        [0, 4],
+        [0, 5],
+        [0, 6],
+        [0, 7],
+        [-1, 0],
+        [-2, 0],
+        [-3, 0],
+        [-4, 0],
+        [-5, 0],
+        [-6, 0],
+        [-7, 0],
+        [0, -1],
+        [0, -2],
+        [0, -3],
+        [0, -4],
+        [0, -5],
+        [0, -6],
+        [0, -7],
+        [1, 1],
+        [2, 2],
+        [3, 3],
+        [4, 4],
+        [5, 5],
+        [6, 6],
+        [7, 7],
+        [1, -1],
+        [2, -2],
+        [3, -3],
+        [4, -4],
+        [5, -5],
+        [6, -6],
+        [7, -7],
+        [-1, 1],
+        [-2, 2],
+        [-3, 3],
+        [-4, 4],
+        [-5, 5],
+        [-6, 6],
+        [-7, 7],
+        [-1, -1],
+        [-2, -2],
+        [-3, -3],
+        [-4, -4],
+        [-5, -5],
+        [-6, -6],
+        [-7, -7],
+    ],
+    'ROOK': [
+        [1, 0],
+        [2, 0],
+        [3, 0],
+        [4, 0],
+        [5, 0],
+        [6, 0],
+        [7, 0],
+        [0, 1],
+        [0, 2],
+        [0, 3],
+        [0, 4],
+        [0, 5],
+        [0, 6],
+        [0, 7],
+        [-1, 0],
+        [-2, 0],
+        [-3, 0],
+        [-4, 0],
+        [-5, 0],
+        [-6, 0],
+        [-7, 0],
+        [0, -1],
+        [0, -2],
+        [0, -3],
+        [0, -4],
+        [0, -5],
+        [0, -6],
+        [0, -7],
+    ],
+    'BISHOP': [
+        [1, 1],
+        [2, 2],
+        [3, 3],
+        [4, 4],
+        [5, 5],
+        [6, 6],
+        [7, 7],
+        [1, -1],
+        [2, -2],
+        [3, -3],
+        [4, -4],
+        [5, -5],
+        [6, -6],
+        [7, -7],
+        [-1, 1],
+        [-2, 2],
+        [-3, 3],
+        [-4, 4],
+        [-5, 5],
+        [-6, 6],
+        [-7, 7],
+        [-1, -1],
+        [-2, -2],
+        [-3, -3],
+        [-4, -4],
+        [-5, -5],
+        [-6, -6],
+        [-7, -7],
+    ],
+    'KNIGHT': [
+        [1, 2],
+        [2, 1],
+        [2, -1],
+        [-1, 2],
+        [-1, -2],
+        [-2, -1],
+        [-2, 1],
+        [-1, 2]
+    ],
+    'PAWN': [
+        [0, 1],
+        [0, 2]
+    ]
+}
+
 export default class ChessPiece {
 
     static TYPE = [
@@ -15,7 +161,9 @@ export default class ChessPiece {
     constructor(type, color) {
         this.type = type;
         this.color = color;
-        this.cell = false;
+        this._cell = false;
+
+        this.possibleMoves = [];
 
         let icon = this.icon = document.createElement('img');
         icon.width = 50;
@@ -32,6 +180,23 @@ export default class ChessPiece {
                 reject(`error loading ${type} icon: ${ev.toString()}`);
             });
         });
+    }
+
+    get cell() {
+        return this._cell;
+    }
+    set cell(cell) {
+        this._cell = cell;
+
+        if(cell) {
+            const max = CONST.cellCount;
+            this.possibleMoves = MOVEMENTS[this.type]
+                .map(m => [m[0] + cell.position[0], m[1] + cell.position[1]])
+                .filter(m => m[0] >= 0 && m[0] < max && m[1] >= 0 && m[1] < max);
+        }
+        else {
+            this.possibleMoves = [];
+        }
     }
 
     /** experiment with dynamically defined method - instances draw a text
